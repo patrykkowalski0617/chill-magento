ce9c1ccbea319f4c91adc202e1a6ad526992d329
Patrycjusz Kowalski <patrykkowalski0617@gmail.com>
2022-08-30 10:33:58 +0200

    listingNew: coupon code autocomplete via defaultFix

--- src/generator/modules/listingNew/actions/defaultFix.js
+++ src/generator/modules/listingNew/actions/defaultFix.js
@@ -10,7 +10,16 @@ const defaultFix = (module) => {
   const maximumItemsInput = module.querySelector(
     "[id^=products_new_max_products_per_category_]"
   );
+  const codeInput = module.querySelector("[id^=products_new_coupon_code_]");
+  const codeInputSource = document.querySelector(
+    "[id^=banner_hero_promo_code_]"
+  );
+
   maximumItemsInput.value = 7;
+  if (codeInputSource) {
+    codeInput.value = codeInputSource.value;
+    markInputs([codeInput]);
+  }
   markInputs([maximumItemsInput]);
 };
 
