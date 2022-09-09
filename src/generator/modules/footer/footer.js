c23e7aa89325efcbe124ae46d74649533a0735ef
Patrycjusz Kowalski <patrykkowalski0617@gmail.com>
2022-09-09 12:43:55 +0200

    footer defaultFix for n24

--- src/generator/modules/footer/footer.js
+++ src/generator/modules/footer/footer.js
@@ -5,7 +5,7 @@ import "./footer.scss";
 
 const footer = moduleFix({
   moduleClass: "module__footer",
-  actions: [{ btnName: "defaultFix", callback: defaultFix }],
+  actions: [{ btnName: "defaultFix", callback: defaultFix, n24Ready: true }],
 });
 
 export default footer;
