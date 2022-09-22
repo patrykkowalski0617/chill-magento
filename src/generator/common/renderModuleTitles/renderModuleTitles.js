ab937471da51b90585f4eef9169d59cd345be1c8
Patrycjusz Kowalski <patrykkowalski0617@gmail.com>
2022-09-22 15:41:00 +0200

    renderModuleTitles add settimeout

--- src/generator/common/renderModuleTitles/renderModuleTitles.js
+++ src/generator/common/renderModuleTitles/renderModuleTitles.js
@@ -1,31 +1,34 @@
 const renderModuleTitles = (providedModule) => {
-  const modules = document.querySelectorAll(".module");
-  const renderTitle = (module) => {
-    const modulOryginalTitle = module.querySelector(".module__title").innerText;
-    const chillHeaderModuleTitle = module.querySelector(
-      ".chill-btn-container .module__title"
-    );
-    if (!chillHeaderModuleTitle) {
-      module
-        .querySelector(".chill-btn-container")
-        .insertAdjacentHTML(
-          "afterbegin",
-          `<span class="module__title"></span>`
-        );
-      module.querySelector(".chill-btn-container .module__title").innerHTML =
-        modulOryginalTitle;
-    } else {
-      chillHeaderModuleTitle.innerHTML = modulOryginalTitle;
-    }
-  };
+  setTimeout(() => {
+    const modules = document.querySelectorAll(".module");
+    const renderTitle = (module) => {
+      const modulOryginalTitle =
+        module.querySelector(".module__title").innerText;
+      const chillHeaderModuleTitle = module.querySelector(
+        ".chill-btn-container .module__title"
+      );
+      if (!chillHeaderModuleTitle) {
+        module
+          .querySelector(".chill-btn-container")
+          .insertAdjacentHTML(
+            "afterbegin",
+            `<span class="module__title"></span>`
+          );
+        module.querySelector(".chill-btn-container .module__title").innerHTML =
+          modulOryginalTitle;
+      } else {
+        chillHeaderModuleTitle.innerHTML = modulOryginalTitle;
+      }
+    };
 
-  if (!providedModule) {
-    modules.forEach((module) => {
-      renderTitle(module);
-    });
-  } else {
-    renderTitle(providedModule);
-  }
+    if (!providedModule) {
+      modules.forEach((module) => {
+        renderTitle(module);
+      });
+    } else {
+      renderTitle(providedModule);
+    }
+  }, 700);
 };
 
-export default setTimeout(renderModuleTitles, 700);
+export default renderModuleTitles;
