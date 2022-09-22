2b81a07ce62afea6bbcef1d5360e5e4e3c5a32d6
Patrycjusz Kowalski <patrykkowalski0617@gmail.com>
2022-09-22 14:17:19 +0200

    banner hero hiddenElements

--- src/generator/modules/bannerHero/actions/index.js
+++ src/generator/modules/bannerHero/actions/index.js
@@ -1,3 +1,4 @@
 export { default as defaultFix } from "./defaultFix";
 export { default as existingModuleCallback } from "./existingModuleCallback";
 export { default as newModuleCallback } from "./newModuleCallback";
+export { default as hideElements } from "./hideElements";
--- src/generator/modules/bannerHero/bannerHero.js
+++ src/generator/modules/bannerHero/bannerHero.js
@@ -4,11 +4,15 @@ import {
   defaultFix,
   existingModuleCallback,
   newModuleCallback,
+  hideElements,
 } from "./actions";
 
 const bannerHero = moduleFix({
   moduleClass: "module__banner_hero",
-  actions: [{ btnName: "defaultFix", callback: defaultFix }],
+  actions: [
+    { btnName: "defaultFix", callback: defaultFix },
+    { btnName: "hideElements", callback: hideElements },
+  ],
   existingModuleCallback,
   newModuleCallback,
 });
