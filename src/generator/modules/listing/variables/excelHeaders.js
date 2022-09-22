f8d0277fc8fed738b83e505203dd7ea68b1fa765
Patrycjusz Kowalski <patrykkowalski0617@gmail.com>
2022-09-22 15:23:35 +0200

    excelHeader bug fix

--- src/generator/modules/listing/variables/excelHeaders.js
+++ src/generator/modules/listing/variables/excelHeaders.js
@@ -2,7 +2,7 @@ const excelHeaders = [
   { gid: ["g", "neoid", , "gid", "neo id"] },
   { proc: ["p", "proc"] },
   { disc: ["r", "rabat"] },
-  { price: ["c", "cena po rabacie", "cena promo", "cena"] },
+  { price: ["c", "cena po rabacie", "cena promo"] },
   { priority: ["l", "pozycja lp", "poz"] },
   { code: ["k", "kod"] },
   { min: ["m", "cena min", "min"] },
