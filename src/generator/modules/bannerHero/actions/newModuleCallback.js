a03031f3052ff05577b07ae829869054778b2785
Patrycjusz Kowalski <patrykkowalski0617@gmail.com>
2022-09-29 10:46:45 +0200

    baneerHero > hideElements - bug fix

--- src/generator/modules/bannerHero/actions/newModuleCallback.js
+++ src/generator/modules/bannerHero/actions/newModuleCallback.js
@@ -1,6 +1,7 @@
 import {
   altsAndDatesForAdditionalImages,
   inputFileFix,
+  markElementsForHideElements,
   stepsFix,
 } from "../variables";
 
@@ -8,6 +9,7 @@ const newModuleCallback = ({ module }) => {
   altsAndDatesForAdditionalImages(module);
   inputFileFix(module);
   stepsFix(module);
+  markElementsForHideElements(module);
 };
 
 export default newModuleCallback;
--- src/generator/modules/bannerHero/variables/index.js
+++ src/generator/modules/bannerHero/variables/index.js
@@ -1,3 +1,4 @@
 export { default as altsAndDatesForAdditionalImages } from "./altsAndDatesForAdditionalImages";
 export { default as inputFileFix } from "./inputFileFix";
 export { default as stepsFix } from "./stepsFix";
+export { default as markElementsForHideElements } from "./markElementsForHideElements";
