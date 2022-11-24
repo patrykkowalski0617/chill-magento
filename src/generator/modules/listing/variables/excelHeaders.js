183b058a6c46e6cd516b84c428bd99f182b528c5
Patrycjusz Kowalski <patrykkowalski0617@gmail.com>
2022-11-24 09:13:24 +0100

    excelHeaders update

--- src/generator/modules/listing/variables/excelHeaders.js
+++ src/generator/modules/listing/variables/excelHeaders.js
@@ -3,7 +3,7 @@ const excelHeaders = [
   { proc: ["p", "proc"] },
   { disc: ["r", "rabat"] },
   { price: ["c", "cena po rabacie", "cena promo"] },
-  { priority: ["l", "pozycja lp", "poz", "kolejność"] },
+  { priority: ["l", "pozycja lp", "poz", "kolejność", "kolejnosc"] },
   { code: ["k", "kod"] },
   { min: ["m", "cena min", "min"] },
 ];
