cc0cca9e76e797842538b92e4f260aedaaf18cba
Patrycjusz Kowalski <patrykkowalski0617@gmail.com>
2022-12-07 12:30:00 +0100

    default fix for bezpiecznik changed

--- src/generator/modules/listing/actions/bezpiecznik.js
+++ src/generator/modules/listing/actions/bezpiecznik.js
@@ -2,17 +2,19 @@ import { markInputs } from "../../../../chill";
 import { selectors } from "../variables";
 
 const bezpiecznik = ({ module }) => {
-  const dateFrom = document.querySelector('[name="date_from"]').value;
-  const timeInput = module.querySelector(selectors.timeInput);
   const dateInput = module.querySelector(selectors.dateInput);
-
-  dateInput.value = `${dateFrom.substr(6, 4)}-${dateFrom.substr(
-    3,
-    2
-  )}-${dateFrom.substr(0, 2)}`;
-  dateInput.classList.add("filled");
-  timeInput.value = dateFrom.substr(11);
-  timeInput.classList.add("filled");
-  markInputs([timeInput, dateInput]);
+  const timeInput = module.querySelector(selectors.timeInput);
+  const date = new Date();
+  const date5minLater = new Date(date.getTime() + 1000 * 60 * 5);
+  const date5minLaterData = {
+    day: String(date5minLater.getDate()).padStart(2, 0),
+    month: String(date5minLater.getMonth() + 1).padStart(2, 0),
+    year: date5minLater.getFullYear(),
+    minutes: String(date5minLater.getMinutes()).padStart(2, 0),
+    houres: String(date5minLater.getHours()).padStart(2, 0),
+  };
+  dateInput.value = `${date5minLaterData.year}-${date5minLaterData.month}-${date5minLaterData.day}`;
+  timeInput.value = `${date5minLaterData.houres}:${date5minLaterData.minutes}`;
+  markInputs([dateInput, timeInput]);
 };
 export default bezpiecznik;
