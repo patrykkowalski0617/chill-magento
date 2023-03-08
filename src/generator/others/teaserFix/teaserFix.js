21411ded8489fb7eaece8cdb9fe54bae1e4da73f
Patrycjusz Kowalski <patrykkowalski0617@gmail.com>
2023-03-08 12:40:54 +0100

    new fixes: teaser, save all modules, tabs

--- src/generator/others/teaserFix/teaserFix.js
+++ src/generator/others/teaserFix/teaserFix.js
@@ -1,3 +1,5 @@
+import { forceChangeEvent } from "../../../chill";
+
 const teaserFix = () => {
   const input = document.querySelector("[name=use_teaser]");
   const label = document.querySelector("[name=use_teaser] + label");
@@ -7,6 +9,7 @@ const teaserFix = () => {
       if (input.checked) {
         const select = document.querySelector("[name=clock_type]");
         select.selectedIndex = 4;
+        forceChangeEvent("[name=clock_type]");
       }
     }, 0);
   });
