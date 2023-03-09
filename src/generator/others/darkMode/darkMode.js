38661a157d1ff1926b3c65069dd08e3c063ed0ce
Patrycjusz Kowalski <patrykkowalski0617@gmail.com>
2023-03-09 14:06:42 +0100

    dark mode

--- src/generator/others/darkMode/darkMode.js
+++ src/generator/others/darkMode/darkMode.js
@@ -2,9 +2,12 @@ import "./darkMode.scss";
 
 const darkMode = () => {
   let darkMode = JSON.parse(localStorage.getItem("magentoChill_darkMode"));
+
   if (darkMode === null) {
     darkMode = false;
     localStorage.setItem("magentoChill_darkMode", darkMode);
+  } else if (darkMode === true) {
+    document.body.classList.add("chill-dark-mode");
   }
 };
 
