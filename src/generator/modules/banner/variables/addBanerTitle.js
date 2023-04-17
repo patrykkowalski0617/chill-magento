3ec5b2309678f05f7591a3d5e405d8c90ce4ddd6
Patrycjusz Kowalski <patrykkowalski0617@gmail.com>
2023-04-17 15:27:07 +0200

    banner title bug fix

--- src/generator/modules/banner/variables/addBanerTitle.js
+++ src/generator/modules/banner/variables/addBanerTitle.js
@@ -1,6 +1,7 @@
 const addBanerTitle = (module) => {
   const banerTitle = module.querySelector('[id^="banner_alt"]').value;
-  const moduleTitleEl = module.querySelectorAll(".module__title");
+  const moduleTitleEl = module.querySelector(".module__title");
+  console.log(banerTitle, moduleTitleEl);
   moduleTitleEl.innerHTML = `BANER ${banerTitle}`;
 };
 
