ed4fbbf52d779786abfe50bd26c04671b280aac3
Patrycjusz Kowalski <patrykkowalski0617@gmail.com>
2023-11-27 14:19:34 +0100

    fix

--- src/others/copyTableCellContent/copyTableCellContent.js
+++ src/others/copyTableCellContent/copyTableCellContent.js
@@ -2,16 +2,19 @@ import "./copyTableCellContent.scss";
 import { copyContent } from "../../chill";
 
 const copyTableCellContent = () => {
-  const table = document.querySelector(".admin__data-grid-outer-wrap");
-
-  table.addEventListener("click", (e) => {
-    if (e.altKey) {
+  const tableDataEls = Array.from(
+    document.querySelectorAll(".admin__data-grid-outer-wrap td")
+  );
+  tableDataEls.forEach((tableDataEl) => {
+    tableDataEl.addEventListener("click", (e) => {
+      if (e.altKey) {
+        copyContent(e.target);
+      }
+    });
+    tableDataEl.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
       copyContent(e.target);
-    }
-  });
-  table.addEventListener("contextmenu", (e) => {
-    e.preventDefault();
-    copyContent(e.target);
+    });
   });
 };
 export default copyTableCellContent;
